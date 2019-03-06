e1ca51cb6c6124a2d0bb1cbf76fc268bb74b5f2d
Michael Benedetti <benedettim@forcenexus.io>
2019-03-05 21:37:53 -0500

    Can customize profiles/characters

--- client/src/GearItem.tsx
+++ client/src/GearItem.tsx
@@ -1,4 +1,3 @@
-import {Component} from "react";
 import * as React from "react";
 import Tooltip from '@material-ui/core/Tooltip';
 
@@ -8,40 +7,33 @@ interface GearItemProps {
   detailedItem: any;
 }
 
-class GearItem extends Component<GearItemProps> {
-  constructor(props: GearItemProps) {
-    super(props);
-  }
+function GearItem(props: GearItemProps) {
+  console.log(props.gearSpot);
+  const itemIcon = props.hero["items"][props.gearSpot] && props.hero["items"][props.gearSpot]["icon"] || "";
+  const item = props.detailedItem;
+  const primaryAttributes = item["attributesHtml"] && item["attributesHtml"]["primary"] || [];
+  const secondaryAttributes = item["attributesHtml"] && item["attributesHtml"]["secondary"] || [];
+  const setDescription = item["set"] && item["set"]["descriptionHtml"] || "";
 
-  render() {
-    const itemIcon = this.props.hero["items"][this.props.gearSpot]["icon"];
-    const item = this.props.detailedItem;
-    const primaryAttributes = item["attributesHtml"]["primary"];
-    const secondaryAttributes = item["attributesHtml"]["secondary"];
-    const setDescription = item["set"] && item["set"]["descriptionHtml"];
+  const tooltip = (
+    <div className={"tooltip"}>
+      <h2>{item["typeName"]}</h2>
+      <h1>{item["name"]}</h1>
+      {primaryAttributes.map((attribute: string) => <div dangerouslySetInnerHTML={{__html: attribute}}/>)}
+      {secondaryAttributes.map((attribute: string) => <div dangerouslySetInnerHTML={{__html: attribute}}/>)}
+      <div dangerouslySetInnerHTML={{__html: setDescription}}/>
+    </div>
+  );
 
-    const tooltip = (
-      <div className={"tooltip"}>
-        <h2>{item["typeName"]}</h2>
-        <h1>{item["name"]}</h1>
-        {primaryAttributes.map((attribute: string) => <div dangerouslySetInnerHTML={{__html: attribute}}/>)}
-        {secondaryAttributes.map((attribute: string) => <div dangerouslySetInnerHTML={{__html: attribute}}/>)}
-        <div dangerouslySetInnerHTML={{__html: setDescription}}/>
+  return (
+      <div className={`box ${props.gearSpot}`}>
+        <Tooltip
+          title={tooltip}
+        >
+          <img alt={props.gearSpot} src={`http://media.blizzard.com/d3/icons/items/large/${itemIcon}.png`}/>
+        </Tooltip>
       </div>
-    );
-
-    return (
-      <>
-        <div className={`box ${this.props.gearSpot}`}>
-          <Tooltip
-            title={tooltip}
-          >
-            <img alt="bleh" src={`http://media.blizzard.com/d3/icons/items/large/${itemIcon}.png`}/>
-          </Tooltip>
-        </div>
-      </>
-    );
-  }
+  );
 }
 
 export default GearItem;
--- client/src/HeroCard.tsx
+++ client/src/HeroCard.tsx
@@ -1,123 +1,112 @@
 import * as  React from 'react';
 import './App.css';
-import {Component} from "react";
 import {AuthRepository, D3Repository} from "./interfaces";
 import GearItem from "./GearItem";
-
-interface HeroCardState {
-  hero: any;
-  items: any;
-}
+import {useEffect, useState} from "react";
+import {Select} from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
 
 interface HeroCardProps {
   authRepository: AuthRepository;
   d3Repository: D3Repository;
   account: string;
   heroid: string;
+  heroIndex: number;
 }
 
-class HeroCard extends Component<HeroCardProps, HeroCardState> {
-  constructor(props: HeroCardProps) {
-    super(props);
-    this.state = {
-      hero: null,
-      items: null,
-    }
+function HeroCard(props: HeroCardProps) {
+  const [hero, setHero] = useState();
+  const [items, setItems] = useState();
+  const [profileInput, setProfileInput] = useState();
+  const [profile, setProfile] = useState();
+  const [heroInput, setHeroInput] = useState();
+  const [accessToken, setAccessToken] = useState();
+
+  const fetchAccessToken = async () => {
+    const fetchedAccessToken: any = await props.authRepository.getAccessToken();
+    setAccessToken(fetchedAccessToken);
+  };
+
+  useEffect(() => {
+    setHeroInput(props.heroid);
+    setProfileInput(props.account);
+    fetchAccessToken();
+  }, []);
+
+  function handleProfileChange(event: any) {
+    setProfileInput(event.target.value);
   }
 
-  public async componentDidMount() {
-    const accessToken: any = await this.props.authRepository.getAccessToken();
-    const hero = await this.props.d3Repository.getHero(this.props.account, this.props.heroid, accessToken);
-    const items = await this.props.d3Repository.getDetailedItems(this.props.account, this.props.heroid, accessToken);
-    this.setState({
-      hero,
-      items,
-    })
+  function handleHeroChange(event: any) {
+    setHeroInput(event.target.value);
   }
 
-  render() {
-    return (
-      <>
+  async function fetchProfile() {
+    const fetchedProfile = await props.d3Repository.getProfile(profileInput, accessToken);
+    setProfile(fetchedProfile);
+  }
+
+  async function fetchHero() {
+    const fetchedHero = await props.d3Repository.getHero(profileInput, heroInput, accessToken);
+    const fetchedItems = await props.d3Repository.getDetailedItems(profileInput, heroInput, accessToken);
+
+    setHero(fetchedHero);
+    setItems(fetchedItems);
+  }
+
+  return (
+    <>
+      <div>
+        <div className="selectors">
+          <input value={profileInput} onChange={handleProfileChange}/>
+          <button onClick={fetchProfile}>submit</button>
+          <br/>
+          {profile && (
+            <>
+              <Select
+                native
+                value={heroInput}
+                onChange={handleHeroChange}
+              >
+                <MenuItem value={""}/>
+                {profile["heroes"].map((hero: any) => {
+                  return (
+                    <option
+                      key={hero["id"]}
+                      value={hero["id"]}
+                    >
+                      {`${hero["name"]} (${hero["class"]})`}
+                    </option>
+                  )
+                })}
+              </Select>
+              <button onClick={fetchHero}>submit</button>
+            </>
+          )}
+        </div>
         {
-          this.state.hero && this.state.items &&(
+          hero && items && (
             <div>
-              {this.state.hero && this.state.hero["name"]}
+              {hero && hero["name"]}
               <div className={"hero-grid"}>
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"head"}
-                  detailedItem={this.state.items["head"]}
-
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"neck"}
-                  detailedItem={this.state.items["neck"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"torso"}
-                  detailedItem={this.state.items["torso"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"shoulders"}
-                  detailedItem={this.state.items["shoulders"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"legs"}
-                  detailedItem={this.state.items["legs"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"waist"}
-                  detailedItem={this.state.items["waist"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"hands"}
-                  detailedItem={this.state.items["hands"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"bracers"}
-                  detailedItem={this.state.items["bracers"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"feet"}
-                  detailedItem={this.state.items["feet"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"leftFinger"}
-                  detailedItem={this.state.items["leftFinger"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"rightFinger"}
-                  detailedItem={this.state.items["rightFinger"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"mainHand"}
-                  detailedItem={this.state.items["mainHand"]}
-                />
-                <GearItem
-                  hero={this.state.hero}
-                  gearSpot={"offHand"}
-                  detailedItem={this.state.items["offHand"]}
-                />
+                {Object.keys(items).map((item: string, i) => {
+                  return (
+                    <GearItem
+                      hero={hero}
+                      gearSpot={item}
+                      detailedItem={items[item]}
+                    />
+                  );
+                })}
               </div>
-              Life: {this.state.hero["stats"]["life"]} <br/>
-              Damage: {this.state.hero["stats"]["damage"]} <br/>
-              Toughness: {this.state.hero["stats"]["toughness"]} <br/>
+              Life: {hero["stats"]["life"]} <br/>
+              Damage: {hero["stats"]["damage"]} <br/>
+              Toughness: {hero["stats"]["toughness"]} <br/>
             </div>
           )}
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default HeroCard;
